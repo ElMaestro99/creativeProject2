c0ecaf79cdcd4341b965ac21a8ceb0355fc738e2
Alec Bingham <asbingham@hotmail.com>
2021-02-26 18:50:21 -0700

    Pokemon type debug

--- script.js
+++ script.js
@@ -20,14 +20,38 @@ document.getElementById("characterSubmit").addEventListener("click", function(ev
       }
       else if (type === "pokemon") {
         document.getElementById("sprite").src = json.sprites.front_default;
-        results += json.id + " " + json.name;
+        var capitalName = capitalizeFirst(json.name);
+        results += "<h2>" + capitalName + "</h2><div class='pkmnTypes'>";
+        for (let i = 0; i < json.types.length; i += 1) {
+          results += "<h3 id='tipes'>" + capitalizeFirst(json.types[i].type.name) + "</h3>"
+        }
+        results += "</div><div class='abilities'><h4 id='tipes'>Known abilities:</h4>";
+        for (let j = 0; j < json.abilities.length; j += 1) {
+          results += "<h4 id='tipes'>" + capitalizeFirst(json.abilities[j].ability.name) + "</h4>";
+        }
+        results += "</div><div class='moveTable'><div id='tableHead'>Learnable moves:</div><div id='tableBody'>";
+        for (let k = 0; k < json.moves.length; k += 1) {
+          results += "<div id='tableElement'>" + capitalizeFirst(json.moves[k].move.name) + "</div>";
+        }
+        results += "</div></div>";
       }
       else if (type === "item") {
         document.getElementById("sprite").src = json.sprites.default;
         results += json.id + " " + json.name + " description: " + json.effect_entries[0].effect;
       }
 
+      document.getElementById("underSprite").innerHTML = results;
+
       console.log(results);
     })
 
 });
+
+function capitalizeFirst(str) {
+  var smallName = str;
+  var firstLetter = smallName.charAt(0);
+  var firstLetterCapital = firstLetter.toUpperCase();
+  var excludeFirst = smallName.slice(1);
+  var capitalName = firstLetterCapital + excludeFirst;
+  return capitalName;
+}
